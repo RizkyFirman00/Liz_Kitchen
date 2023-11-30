d13f6debdd1b2c3990f58b5fbe7bc6df1d4ee572
RizkyFirman00 <firmansyah.mrf@gmail.com>
2023-11-30 15:35:37 +0700

    bikin user page
    Next :
    tampilin order pake history

---
+++ app/src/main/java/com/dissy/lizkitchen/ui/admin/AdminHomeActivity.kt
@@ -0,0 +1,28 @@
+package com.dissy.lizkitchen.ui.admin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dissy.lizkitchen.databinding.ActivityAdminHomeBinding
+
+class AdminHomeActivity : AppCompatActivity() {
+    private val binding by lazy {ActivityAdminHomeBinding.inflate(layoutInflater)}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnToCakes.setOnClickListener {
+            Intent(this, AdminCakeActivity::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        }
+
+        binding.btnToUsers.setOnClickListener {
+            Intent(this, AdminUserActivity::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        }
+    }
+}
